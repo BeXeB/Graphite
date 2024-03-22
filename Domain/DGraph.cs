16f17558cc443bd596ae051dcece6c04b9e5adb0
Simeon Kolev <simeonkolev99@gmail.com>
2024-03-22 22:48:49 +0100

    Moved tag related methods to abstract class
    Tagging logic can be reused across different types of graphs. (I believe)

--- Domain/DGraph.cs
+++ Domain/DGraph.cs
@@ -42,57 +42,5 @@ namespace Domain
                 }
             }
         }
-
-        public override void AddTags(Predicate<List<string>> pred, List<string> tags)
-        {
-            var indexes = GetVertices(pred);
-
-            foreach (var index in indexes)
-            {
-                foreach (var tag in tags)
-                {
-                    if (!Tags[index].Contains(tag))
-                    {
-                        Tags[index].Add(tag);
-                    }
-                }
-            }
-        }
-
-        public override void RemoveTags(Predicate<List<string>> pred, List<string> tags)
-        {
-            var indexes = GetVertices(pred);
-
-            foreach (var index in indexes)
-            {
-                foreach (var tag in tags)
-                {
-                    Tags[index].RemoveAll(t => t == tag);
-                }
-            }
-        }
-
-        public override void Retag(string from, string to)
-        {
-            var newTags = new List<List<string>>();
-
-            foreach (var vertex in Tags)
-            {
-                var newVertex = new List<string>();
-                foreach (var tag in vertex)
-                {
-                    if (tag == from)
-                    {
-                        newVertex.Add(to);
-                    }
-                    else
-                    {
-                        newVertex.Add(tag);
-                    }
-                }
-                newTags.Add(newVertex);
-            }
-            Tags = newTags;
-        }
     }
 }
--- Domain/Graph.cs
+++ Domain/Graph.cs
@@ -9,13 +9,63 @@
         public abstract void AddVertex(string[] vertexTags);
         public abstract void Connect(Predicate<List<string>> fromPred, Predicate<List<string>> toPred);
         public abstract void Disconnect(Predicate<List<string>> fromPred, Predicate<List<string>> toPred);
-        public abstract void AddTags(Predicate<List<string>> pred, List<string> tags);
-        public abstract void RemoveTags(Predicate<List<string>> pred, List<string> tags);
-        public abstract void Retag(string from, string to);
 
         public void AddVertices(List<string[]> vertexTags) => vertexTags.ForEach(AddVertex);
+
         public int[] GetVertices(Predicate<List<string>> pred)
             => Enumerable.Range(0, Tags.Count).Where(i => pred(Tags[i])).ToArray();
+
+        public void Retag(string from, string to)
+        {
+            var newTags = new List<List<string>>();
+
+            foreach (var vertex in Tags)
+            {
+                var newVertex = new List<string>();
+                foreach (var tag in vertex)
+                {
+                    if (tag == from)
+                    {
+                        newVertex.Add(to);
+                    }
+                    else
+                    {
+                        newVertex.Add(tag);
+                    }
+                }
+                newTags.Add(newVertex);
+            }
+            Tags = newTags;
+        }
+
+        public void AddTags(Predicate<List<string>> pred, List<string> tags)
+        {
+            var indexes = GetVertices(pred);
+
+            foreach (var index in indexes)
+            {
+                foreach (var tag in tags)
+                {
+                    if (!Tags[index].Contains(tag))
+                    {
+                        Tags[index].Add(tag);
+                    }
+                }
+            }
+        }
+
+        public void RemoveTags(Predicate<List<string>> pred, List<string> tags)
+        {
+            var indexes = GetVertices(pred);
+
+            foreach (var index in indexes)
+            {
+                foreach (var tag in tags)
+                {
+                    Tags[index].RemoveAll(t => t == tag);
+                }
+            }
+        }
 
         public void PrintGraphInfo()
         {

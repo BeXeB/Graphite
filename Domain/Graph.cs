b54e7d3a849fc5cfec6f5c789cd799f7862b48a3
Simeon Kolev <simeonkolev99@gmail.com>
2024-03-22 22:41:05 +0100

    Print graph info method
    Print graph info to console in a somewhat nicer format rather than having to go into debug mode

--- Domain/Graph.cs
+++ Domain/Graph.cs
@@ -16,6 +16,30 @@
         public void AddVertices(List<string[]> vertexTags) => vertexTags.ForEach(AddVertex);
         public int[] GetVertices(Predicate<List<string>> pred)
             => Enumerable.Range(0, Tags.Count).Where(i => pred(Tags[i])).ToArray();
+
+        public void PrintGraphInfo()
+        {
+            Console.WriteLine("Graph Information and Adjacency Matrix: " + GetType().Name);
+            Console.WriteLine("+-----------+-------------------------------+------------------+");
+            Console.WriteLine("| Vertex    | Tags                          | Adjacency Matrix |");
+            Console.WriteLine("+-----------+-------------------------------+------------------+");
+
+            for (int i = 0; i < NoOfVertices; i++)
+            {
+                string vertex = $"| {i + 1,-9} | [{string.Join(", ", Tags[i])}]";
+
+                string adjacencyMatrixRow = "| ";
+                for (int j = 0; j < NoOfVertices; j++)
+                {
+                    adjacencyMatrixRow += $"{(AdjMatrix[i][j].ToString() == "True" ? "T" : "-"),-4} ";
+                }
+                adjacencyMatrixRow += "|";
+
+                Console.WriteLine($"{vertex,-44}{adjacencyMatrixRow,-17}");
+            }
+
+            Console.WriteLine("+-----------+-------------------------------+-----------------+");
+        }
     }
 
     public interface IGraph

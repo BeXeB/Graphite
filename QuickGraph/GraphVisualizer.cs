1814a23f74c7d96efceed7eec686ab9f26668876
Simeon Kolev <simeonkolev99@gmail.com>
2024-03-24 00:30:01 +0100

    GraphVisualizer also for UGraph

--- QuickGraph/GraphVisualizer.cs
+++ QuickGraph/GraphVisualizer.cs
@@ -8,10 +8,11 @@ namespace QuikGraphVisualizer
 {
     public static class GraphVisualizer<T>
     {
+        private const string GraphvizOnlineBaseUrl = "https://dreampuf.github.io/GraphvizOnline/#";
+
         public static void VisualizeInBrowser(Graph<T> graph)
         {
-            var quikGraph = ToQuikGraph(graph);
-            var graphviz = new GraphvizAlgorithm<string, Edge<string>>(quikGraph);
+            var graphviz = new GraphvizAlgorithm<string, Edge<string>>(graph is DGraph ? ToQuikDGraph(graph) : ToQuikUGraph(graph));
 
             // Customize vertex appearance
             graphviz.FormatVertex += (sender, args) =>
@@ -34,9 +35,7 @@ namespace QuikGraphVisualizer
 
             var dotGraph = graphviz.Generate();
 
-            var baseUrl = "https://dreampuf.github.io/GraphvizOnline/#";
-
-            var url = $"{baseUrl}{Uri.EscapeDataString(dotGraph)}";
+            var url = $"{GraphvizOnlineBaseUrl}{Uri.EscapeDataString(dotGraph)}";
 
             Process.Start(new ProcessStartInfo
             {
@@ -45,14 +44,14 @@ namespace QuikGraphVisualizer
             });
         }
 
-        private static BidirectionalGraph<string, Edge<string>> ToQuikGraph(Graph<T> graph)
+        private static BidirectionalGraph<string, Edge<string>> ToQuikDGraph(Graph<T> graph)
         {
             var quikGraph = new BidirectionalGraph<string, Edge<string>>();
 
             // Add vertices
             for (int i = 0; i < graph.NoOfVertices; i++)
             {
-                quikGraph.AddVertex($"Vertex {i}");
+                quikGraph.AddVertex(string.Join(", ", graph.Tags[i]));
             }
 
             // Add edges
@@ -62,7 +61,34 @@ namespace QuikGraphVisualizer
                 {
                     if (!graph.AdjMatrix[i][j].Equals(default(T))) // Assuming default(T) means no edge
                     {
-                        quikGraph.AddEdge(new Edge<string>($"Vertex {i}", $"Vertex {j}"));
+                        quikGraph.AddEdge(new Edge<string>(string.Join(", ", graph.Tags[i]), string.Join(", ", graph.Tags[j])));
+                    }
+                }
+            }
+
+            return quikGraph;
+        }
+
+        private static UndirectedGraph<string, Edge<string>> ToQuikUGraph(Graph<T> graph)
+        {
+            var quikGraph = new UndirectedGraph<string, Edge<string>>();
+
+            // Add vertices
+            for (int i = 0; i < graph.NoOfVertices; i++)
+            {
+                quikGraph.AddVertex(string.Join(", ", graph.Tags[i]));
+            }
+
+            // Add edges
+            for (int i = 0; i < graph.NoOfVertices; i++)
+            {
+                for (int j = 0; j < graph.NoOfVertices; j++)
+                {
+                    if (!graph.AdjMatrix[i][j].Equals(default(T))) // Assuming default(T) means no edge
+                    {
+                        if (quikGraph.TryGetEdge(string.Join(", ", graph.Tags[j]), string.Join(", ", graph.Tags[i]), out _))
+                            continue;
+                        quikGraph.AddEdge(new Edge<string>(string.Join(", ", graph.Tags[i]), string.Join(", ", graph.Tags[j])));
                     }
                 }
             }

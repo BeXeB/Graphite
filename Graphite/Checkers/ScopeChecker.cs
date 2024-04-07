e434e9b68529412e2caf2c528cc19dced1122fa7
Doma <sari.dominik.cool@gmail.com>
2024-04-07 11:36:20 +0200

    Added scope and type checker skeleton

---
+++ Graphite/Checkers/ScopeChecker.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Graphite.Parser;
+
+namespace Graphite.Checkers
+{
+    internal class ScopeChecker :
+        Statement.IStatementVisitor<object>,
+        Expression.IExpressionVisitor<object>,
+        OtherNonTerminals.IOtherNonTerminalsVisitor<object>,
+        GraphExpression.IGraphExpressionVisitor<object>
+    {
+
+
+        public object VisitAnonFunctionExpression(Expression.AnonFunctionExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitAssignmentExpression(Expression.AssignmentExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitBinaryExpression(Expression.BinaryExpression expression)
+        {
+            
+
+
+            throw new NotImplementedException();
+        }
+
+        public object VisitBlockStatement(Statement.BlockStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitBreakStatement(Statement.BreakStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitCallExpression(Expression.CallExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitClassDeclarationStatement(Statement.ClassDeclarationStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitContinueStatement(Statement.ContinueStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitElementAccessExpression(Expression.ElementAccessExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitExpressionStatement(Statement.ExpressionStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitFunctionDeclarationStatement(Statement.FunctionDeclarationStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitGetFieldExpression(Expression.GetFieldExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitGraphAddVertexExpression(GraphExpression.GraphAddVertexExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitGraphBlockStmt(GraphExpression.GraphBlockStatement expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitGraphEdgeExpression(GraphExpression.GraphEdgeExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitGraphExpressionStmt(GraphExpression.GraphExpressionStatement expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitGraphIfStmt(GraphExpression.GraphIfStatement expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitGraphRemoveVertexExpression(GraphExpression.GraphRemoveVertexExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitGraphReTagExpression(GraphExpression.GraphReTagExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitGraphStatement(Statement.GraphStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitGraphTagExpression(GraphExpression.GraphTagExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitGraphWhileStmt(GraphExpression.GraphWhileStatement expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitGroupingExpression(Expression.GroupingExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitIfStatement(Statement.IfStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitInstanceExpression(Expression.InstanceExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitListExpression(Expression.ListExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitLiteralExpression(Expression.LiteralExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitLogicalExpression(Expression.LogicalExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitParameters(OtherNonTerminals.Parameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitPredicateAndExpression(GraphExpression.PredicateAndExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitPredicateGroupingExpression(GraphExpression.PredicateGroupingExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitPredicateLiteralExpression(GraphExpression.PredicateLiteralExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitPredicateOrExpression(GraphExpression.PredicateOrExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitPredicateUnaryExpression(GraphExpression.PredicateUnaryExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitReturnStatement(Statement.ReturnStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitSetExpression(Expression.SetExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitSetFieldExpression(Expression.SetFieldExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitSuperExpression(Expression.SuperExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitThisExpression(Expression.ThisExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitType(OtherNonTerminals.Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitUnaryExpression(Expression.UnaryExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitVariableDeclarationStatement(Statement.VariableDeclarationStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitVariableExpression(Expression.VariableExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object VisitWhileStatement(Statement.WhileStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Graphite/Checkers/TypeChecker.cs
@@ -0,0 +1,234 @@
+﻿using Graphite.Parser;
+
+namespace Graphite.Checkers
+{
+    internal class TypeChecker :
+        Statement.IStatementVisitor<OtherNonTerminals.Type>,
+        Expression.IExpressionVisitor<OtherNonTerminals.Type>,
+        OtherNonTerminals.IOtherNonTerminalsVisitor<OtherNonTerminals.Type>,
+        GraphExpression.IGraphExpressionVisitor<OtherNonTerminals.Type>
+    {
+        public OtherNonTerminals.Type VisitAnonFunctionExpression(Expression.AnonFunctionExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitAssignmentExpression(Expression.AssignmentExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitBinaryExpression(Expression.BinaryExpression expression)
+        {
+            expression.left.Accept
+
+
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitBlockStatement(Statement.BlockStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitBreakStatement(Statement.BreakStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitCallExpression(Expression.CallExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitClassDeclarationStatement(Statement.ClassDeclarationStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitContinueStatement(Statement.ContinueStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitElementAccessExpression(Expression.ElementAccessExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitExpressionStatement(Statement.ExpressionStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitFunctionDeclarationStatement(Statement.FunctionDeclarationStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitGetFieldExpression(Expression.GetFieldExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitGraphAddVertexExpression(GraphExpression.GraphAddVertexExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitGraphBlockStmt(GraphExpression.GraphBlockStatement expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitGraphEdgeExpression(GraphExpression.GraphEdgeExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitGraphExpressionStmt(GraphExpression.GraphExpressionStatement expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitGraphIfStmt(GraphExpression.GraphIfStatement expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitGraphRemoveVertexExpression(GraphExpression.GraphRemoveVertexExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitGraphReTagExpression(GraphExpression.GraphReTagExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitGraphStatement(Statement.GraphStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitGraphTagExpression(GraphExpression.GraphTagExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitGraphWhileStmt(GraphExpression.GraphWhileStatement expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitGroupingExpression(Expression.GroupingExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitIfStatement(Statement.IfStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitInstanceExpression(Expression.InstanceExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitListExpression(Expression.ListExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitLiteralExpression(Expression.LiteralExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitLogicalExpression(Expression.LogicalExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitParameters(OtherNonTerminals.Parameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitPredicateAndExpression(GraphExpression.PredicateAndExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitPredicateGroupingExpression(GraphExpression.PredicateGroupingExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitPredicateLiteralExpression(GraphExpression.PredicateLiteralExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitPredicateOrExpression(GraphExpression.PredicateOrExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitPredicateUnaryExpression(GraphExpression.PredicateUnaryExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitReturnStatement(Statement.ReturnStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitSetExpression(Expression.SetExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitSetFieldExpression(Expression.SetFieldExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitSuperExpression(Expression.SuperExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitThisExpression(Expression.ThisExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitType(OtherNonTerminals.Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitUnaryExpression(Expression.UnaryExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitVariableDeclarationStatement(Statement.VariableDeclarationStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitVariableExpression(Expression.VariableExpression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OtherNonTerminals.Type VisitWhileStatement(Statement.WhileStatement statement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

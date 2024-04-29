19d306fd1cab2b7c1ed0508d3aa0a31847d64006
Bence Imre Kiss <kissbenceimre99@gmail.com>
2024-04-29 12:12:42 +0200

    Change Program.cs back

--- Graphite/Program.cs
+++ Graphite/Program.cs
@@ -4,56 +4,26 @@
 using System.Reflection;
 using Graphite.Lexer;
 using Graphite.Parser;
-using Domain;
+using Graphite;
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+        var path = basePath.Remove(basePath.IndexOf("Graphite", StringComparison.Ordinal)) + @"Graphite\Graphite\code.txt";
+        var code = File.ReadAllText(path);
 
-// internal class Program
-// {
-//     public static void Main(string[] args)
-//     {
-//         var basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
-//         var path = basePath.Remove(basePath.IndexOf("Graphite", StringComparison.Ordinal)) + @"Graphite\Graphite\code.txt";
-//         var code = File.ReadAllText(path);
-//
-//         var lexer = new Lexer();
-//         var parser = new Parser();
-//         var transpiler = new Transpiler();
-//
-//         var tokens = lexer.ScanCode(code);
-//         var statements = parser.Parse(tokens);
-//         var cscode = transpiler.Transpile(statements);
-//
-//         var outputPath = basePath.Remove(basePath.IndexOf("Graphite", StringComparison.Ordinal)) + @"Graphite\Graphite\output.cs";
-//         File.WriteAllText(outputPath, cscode);
-//     }
-// }
+        var lexer = new Lexer();
+        var parser = new Parser();
+        var transpiler = new Transpiler();
 
-// var dgraph = new DGraph();
-// dgraph.AddVertex(["a", "b"]);
-// dgraph.AddVertex(["c", "d"]);
-// dgraph.AddVertices([["f", "g"], ["h", "i"]]);
-// dgraph.Retag("a", "e");
-// dgraph.AddTags(v => v.Contains("e") || v.Contains("h"), ["j", "i"]);
-// dgraph.RemoveTags(v => v.Contains("j") || v.Contains("e"), ["j", "b"]);
-// dgraph.Connect(v => v.Contains("e"), v => v.Contains("c"));
-// dgraph.Connect(v => v.Contains("f"), v => v.Contains("h"));
-// dgraph.Connect(v => v.Contains("e"), v => v.Contains("h"));
-//
-// var ugraph = new UGraph();
-// ugraph.AddVertex(["a", "b"]);
-// ugraph.AddVertex(["c", "d"]);
-// ugraph.AddVertex(["asd"], 5);
-// ugraph.AddVertices([["f", "g"], ["h", "i"]]);
-// ugraph.Retag("a", "e");
-// ugraph.AddTags(v => v.Contains("e") || v.Contains("h"), ["j", "i"]);
-// ugraph.RemoveTags(v => v.Contains("j") || v.Contains("e"), ["j", "b"]);
-// ugraph.Connect(v => v.Contains("e"), v => v.Contains("c"), 123);
-// ugraph.Connect(v => v.Contains("f"), v => v.Contains("h"), 123.123);
-// ugraph.PrintGraphInfo();
-// ugraph.RemoveVertex(v => v.Contains("asd"));
-//
-// ugraph.PrintGraphInfo();
-// dgraph.PrintGraphInfo();
+        var tokens = lexer.ScanCode(code);
+        var statements = parser.Parse(tokens);
+        var cscode = transpiler.Transpile(statements);
 
+        var outputPath = basePath.Remove(basePath.IndexOf("Graphite", StringComparison.Ordinal)) + @"Graphite\Graphite\output.cs";
+        File.WriteAllText(outputPath, cscode);
+    }
+}
 
-// Uncomment to visualize the graph in the browser
 //GraphVisualizer<bool>.VisualizeInBrowser(dgraph);
